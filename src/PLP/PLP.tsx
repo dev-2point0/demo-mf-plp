9b7ff181b9e3cdb660195f016e29a43a3b215a6c
Sintu Singh <sintu.singh@Sintus-MacBook-Pro.local>
2023-03-08 00:17:08 -0500

    Add Api call example

--- src/PLP/PLP.tsx
+++ src/PLP/PLP.tsx
@@ -1,64 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './PLP.scss';
 
-const PLP = () => {
-    return (<div className="plp-comp">
-        <div className="row">
-            <div className="col">
-                <div className="product-name">
-                    Product 1
-                </div>
-                <div className="product-detail">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                when an unknown printer took a galley of type and scrambled it to make a
-                type specimen book. It has survived not only five centuries,
-                but also the leap into electronic typesetting, remaining essentially unchanged.
-                It was popularised in the 1960s with the release
-                </div>
-            </div>
-            <div className="col">
-                <div className="product-name">
-                    Product 2
-                </div>
-                <div className="product-detail">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                when an unknown printer took a galley of type and scrambled it to make a
-                type specimen book. It has survived not only five centuries,
-                but also the leap into electronic typesetting, remaining essentially unchanged.
-                It was popularised in the 1960s with the release
+class PLP extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            textList: []
+        }
+    }
+    componentDidMount(): void {
+        fetch('https://baconipsum.com/api/?type=all')
+        .then(x => x.json())
+        .then(y => {
+            this.setState({
+                textList: y
+            })
+        });   
+    }
+    render() {
+        const { textList } = this.state;
+        return (<div className="plp-comp">
+            <div className="row">
+                <div className="col plp-text">
+                    {
+                        textList.map((item: string, index: number) => <p key={index}>{item}</p>)   
+                    }
                 </div>
             </div>
-            <div className="col">
-                <div className="product-name">
-                    Product 3
+            <div className="row">
+                <div className="col">
+                    <div className="product-name">
+                        Product 1
+                    </div>
+                    <div className="product-detail">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    when an unknown printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining essentially unchanged.
+                    It was popularised in the 1960s with the release
+                    </div>
                 </div>
-                <div className="product-detail">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                when an unknown printer took a galley of type and scrambled it to make a
-                type specimen book. It has survived not only five centuries,
-                but also the leap into electronic typesetting, remaining essentially unchanged.
-                It was popularised in the 1960s with the release
+                <div className="col">
+                    <div className="product-name">
+                        Product 2
+                    </div>
+                    <div className="product-detail">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    when an unknown printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining essentially unchanged.
+                    It was popularised in the 1960s with the release
+                    </div>
                 </div>
+                <div className="col">
+                    <div className="product-name">
+                        Product 3
+                    </div>
+                    <div className="product-detail">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    when an unknown printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining essentially unchanged.
+                    It was popularised in the 1960s with the release
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="product-name">
+                        Product 4
+                    </div>
+                    <div className="product-detail">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    when an unknown printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining essentially unchanged.
+                    It was popularised in the 1960s with the release
+                    </div>
+                </div>
+                
             </div>
-            <div className="col">
-                <div className="product-name">
-                    Product 4
-                </div>
-                <div className="product-detail">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                when an unknown printer took a galley of type and scrambled it to make a
-                type specimen book. It has survived not only five centuries,
-                but also the leap into electronic typesetting, remaining essentially unchanged.
-                It was popularised in the 1960s with the release
-                </div>
-            </div>
-            
-        </div>
-    </div>);
-};
+        </div>);
+    };
+}
 
 export default PLP;
